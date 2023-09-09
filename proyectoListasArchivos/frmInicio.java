479c4e648542e1e0822deafd62bdd9e3ac579098
jegiraldp <jegiraldp@gmail.com>
2023-09-09 12:14:35 -0500

    090923

--- proyectoListasArchivos/frmInicio.java
+++ proyectoListasArchivos/frmInicio.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class frmInicio extends JFrame implements ActionListener{
     static JButton btnListar, btnRegistrar;
-    static JLabel lblTitulo, lblLogo;
+    static JLabel lblTitulo, lblLogo, lblInformacion;
     static ImageIcon imgLogo, imgRegistrar, imgListar;
 
     public frmInicio(){
@@ -12,6 +12,11 @@ public class frmInicio extends JFrame implements ActionListener{
         imgLogo = new ImageIcon(".\\img\\logo.png");
         imgRegistrar = new ImageIcon(".\\img\\add.png");
         imgListar = new ImageIcon(".\\img\\list.png");
+
+        lblTitulo = new JLabel("Sistema de contactos");
+        lblTitulo.setLocation(125,20);
+        lblTitulo.setSize(200,30);
+        lblTitulo.setFont(new Font("Arial", Font.PLAIN, 18));
 
         btnRegistrar = new JButton("Nuevo", imgRegistrar);
         btnRegistrar.setLocation(100,60);
@@ -25,17 +30,26 @@ public class frmInicio extends JFrame implements ActionListener{
         lblLogo.setLocation(100,80);
         lblLogo.setSize(210,200);
 
+        lblInformacion = new JLabel("");
+        lblInformacion.setForeground(Color.RED);
+        lblInformacion.setLocation(100,270);
+        lblInformacion.setSize(100,30);
+
+
+
         //add
+        add(lblTitulo);
         add(btnListar);
         add(btnRegistrar);
         add(lblLogo);
+        add(lblInformacion);
 
         //set
         Color c = new Color(173, 216, 230);
         getContentPane().setBackground(c);
 
         setLayout(null);
-        setSize(450,300);
+        setSize(450,350);
         setTitle("Agenda de contactos");
 
         setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
