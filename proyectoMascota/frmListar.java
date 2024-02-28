1f39c4c316cf1cfd0fe14bd15c7396e659b653e6
jegiraldp <jegiraldp@gmail.com>
2024-02-27 20:26:45 -0500

    270224

--- proyectoMascota/frmListar.java
+++ proyectoMascota/frmListar.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,13 +7,17 @@ import java.awt.event.ActionListener;
 public class frmListar extends JFrame implements ActionListener {
     static JButton btnRegresar;
     static ImageIcon imgRegresar;
+    static JTable tabla;
+    static DefaultTableModel model;
+    static JScrollPane scrollpane;
 
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(btnRegresar)){
             frmMascotas.ventana.setVisible(true);
             this.setVisible(false);
-        }
+
+                    }
     }
     public frmListar(){
         imgRegresar = new ImageIcon(".\\img\\back.png");
@@ -20,9 +25,21 @@ public class frmListar extends JFrame implements ActionListener {
         btnRegresar.setBounds(50,50,150,30);
         btnRegresar.addActionListener(this);
 
+
+        model = new DefaultTableModel();
+        tabla = new JTable(model);
+
+        model.addColumn("ID");
+        model.addColumn("Nombre");
+
+        scrollpane = new JScrollPane(tabla);
+        scrollpane.setLocation(40,80);
+        scrollpane.setSize(100,100);
+
+
         //
         add(btnRegresar);
-
+        add(scrollpane, BorderLayout.CENTER);
 
         Color c = new Color(173, 216, 230);
         getContentPane().setBackground(c);
@@ -33,6 +50,12 @@ public class frmListar extends JFrame implements ActionListener {
 
         setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         setLocation(300,200);
+
+        Object[] fila1 = {1,"firulais"};
+        Object[] fila2 = {2,"lucky"};
+        model.addRow(fila1);
+        model.addRow(fila2);
+
 
     }
 
