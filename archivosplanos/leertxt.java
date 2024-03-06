6aa09ad51244b62dca16ecb317bca342716d8013
jegiraldp <jegiraldp@gmail.com>
2024-03-05 19:53:02 -0500

    05032024V2

--- archivosplanos/leertxt.java
+++ archivosplanos/leertxt.java
@@ -3,21 +3,20 @@ import java.io.*;
 public class leertxt {
 
     public static void main(String[] args) {
-        String nombreArchivo = "archivo.txt";
+        String nombreArchivo = "C:\\Users\\jorge\\Documents\\cienciaDatos\\archivo.txt";
 
         try {
             FileReader fileReader = new FileReader(nombreArchivo);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String linea;
 
-            linea = bufferedReader.readLine();
-
-            System.out.println(linea);
-
-
+            String linea = bufferedReader.readLine();
+            while(linea!=null) {
+                System.out.println(linea);
+                linea = bufferedReader.readLine();
+            }
             bufferedReader.close();
         } catch (IOException e) {
-            System.out.println("Error tipo IO");
+            System.out.println("Error en lectura");
         }
     }
 }
