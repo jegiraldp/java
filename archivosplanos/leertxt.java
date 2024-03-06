235900fa8f64e1fc74e3168d1a119d00f7131c52
jegiraldp <jegiraldp@gmail.com>
2024-03-05 19:27:27 -0500

    05032024

--- archivosplanos/leertxt.java
+++ archivosplanos/leertxt.java
@@ -8,18 +8,11 @@ public class leertxt {
         try {
             FileReader fileReader = new FileReader(nombreArchivo);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-
             String linea;
-            String[] n = new String[10];
+
             linea = bufferedReader.readLine();
-            String[] plantas = linea.split(";");
-            String[] planta=null;
-            for (int i = 0; i <plantas.length ; i++) {
-                planta=plantas[i].split(",");
-                System.out.println(planta[1]);
-                //planta p = new planta(planta[0],planta[1])
-                //lista.add(p)
-            }
+
+            System.out.println(linea);
 
 
             bufferedReader.close();
@@ -28,4 +21,3 @@ public class leertxt {
         }
     }
 }
-
