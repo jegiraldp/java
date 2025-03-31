6b01f157b0e062a0cd3c48f28bf88c878c354eb6
jegiraldp <jegiraldp@gmail.com>
2025-03-31 16:23:04 -0500

    Commit Repository Pattern

--- patrones/repository/databaseuno/main/InicioDatabase.java
+++ patrones/repository/databaseuno/main/InicioDatabase.java
@@ -1,6 +1,7 @@
 package repository.databaseuno.main;
 
 import repository.databaseuno.util.DatabaseConnection;
+import repository.databaseuno.model.Employee;
 import repository.databaseuno.repository.EmployeeRepository;
 import repository.databaseuno.repository.iRepository;
 
@@ -9,7 +10,18 @@ public class InicioDatabase {
     public static void main(String[] args) {
         System.out.println(DatabaseConnection.getDatabaseConnection());
         iRepository repo = new EmployeeRepository();
+        System.out.println("-----------------FindAll");
         repo.findAll().forEach(System.out::println);
+        System.out.println("-----------------getById");
+        System.out.println(repo.getById(1));
+        System.out.println("-----------------Save");
+        Employee e = new Employee(3,"J3","G3","jegiraldp3@gmail.com",20000);
+        repo.save(e);
+        repo.findAll().forEach(System.out::println);
+        System.out.println("-----------------Delete");
+        repo.delete(2);
+        repo.findAll().forEach(System.out::println);
+        
     }
     
 }
--- patrones/repository/databaseuno/repository/EmployeeRepository.java
+++ patrones/repository/databaseuno/repository/EmployeeRepository.java
@@ -5,42 +5,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 import repository.databaseuno.model.Employee;
-import repository.databaseuno.util.DatabaseConnection;
 
 public class EmployeeRepository implements iRepository<Employee> {
-    private List<Employee> lista = new ArrayList<>();
+    private List<Employee> lista;
+   
 
-    public String getConnection(){
-        return DatabaseConnection.getDatabaseConnection();
+    public EmployeeRepository() {
+        
+        this.lista = new ArrayList<>();
+        lista.add(new Employee(1,"J1","G1","jegiraldp1@gmail.com",10000));
+        lista.add(new Employee(2,"J2","G2","jegiraldp2@gmail.com",20000));
     }
 
     @Override
     public List<Employee> findAll() {
-        List<Employee> lista = new ArrayList<>();
-       lista.add(new Employee(1,"J1","G1","jegiraldp1@gmail.com",10000));
-       lista.add(new Employee(2,"J2","G2","jegiraldp2@gmail.com",20000));
-       return lista;
-       
+     return lista;
     }
 
     @Override
     public Employee getById(int id) {
         for (Employee e : lista) {
-            if(e.getCode()==id) return e;
+           if(e.getCode()==id){
+            return e;
+           }
         }
         return null;
     }
 
     @Override
     public void save(Employee t) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        System.out.println("Saved "+t.toString());
+        lista.add(t);
     }
 
     @Override
     public void delete(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        for (Employee employee : lista) {
+            if(employee.getCode()==id) lista.remove(employee);
+        }
     }
 
     
